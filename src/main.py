dfe9c64f2ebfcaa43f35dc389fd37734cf649f19
DanielQuiramUM <d.quiram@student.maastrichtuniversity.nl>
2025-03-14 20:21:26 +0100

    Added atva3

--- src/main.py
+++ src/main.py
@@ -1,17 +1,28 @@
 from btva import BTVA
 from atva_multiple_voting import ATVA_MV
+from atva_imperfect_knowledge import ATVA_IK
+
 
 def main():
-    n = 3
-    m = 3
+    n = 5
+    m = 5
     
     btva= BTVA(n, m)
     btva.setup()
 
     tuple = btva.display_results("anti_plurality")
     risk = btva.display_risk(tuple)
-    
-    
+
+    ##############################
+    # ATVA - imperfect knowledge
+    ##############################
+
+    #ATVA_3 = ATVA_IK(n, m)
+    #ATVA_3.setup()
+
+    #ATVA_3.display_results("anti_plurality")
+
+
     ##############################
     # ATVA - multiple voting
     ##############################
@@ -20,6 +31,10 @@ def main():
     #ATVA_4.setup()
     
     #ATVA_4.display_results("anti_plurality")
+
+
+
+
 
     
 
---
+++ src/voting_setup.py
@@ -0,0 +1,12 @@
+import random
+from preference_matrix import PreferenceMatrix
+
+class VotingSetup:
+    @staticmethod
+    def generate_random_matrix(n, m):
+        alternatives = [chr(65 + i) for i in range(m)]
+        votes = []
+        for _ in range(n):
+            vote = random.sample(alternatives, len(alternatives))
+            votes.append(vote)
+        return PreferenceMatrix(votes)
